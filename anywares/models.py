c0a78bb5e3a6cca8d0281701248fbc67bbb2bdb3
nickszhu <shihang.zhu@mail.mcgill.ca>
2017-11-15 15:03:03 -0500

    Revert "Ryans branch"

--- anywares/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.IntegerField(default=0)
