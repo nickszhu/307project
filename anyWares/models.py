99970ec0cbb87a0e9b3923608b09683f7ca855ca
Ryan Bass <stripedbass511@gmail.com>
2017-11-27 14:23:35 -0500

    Created ItemView template

--- anyWares/models.py
+++ anyWares/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from mysite.settings import MEDIA_ROOT
 
 class User_Profile(models.Model):
     user = models.ForeignKey(User)
@@ -37,6 +38,8 @@ class Category(models.Model):
         return self.category_name
     
 class Item(models.Model):
+    def generate_upload_path(self, filename):
+        return 'static/anyWares/media/%s' % filename
     name = models.CharField(max_length=20)
     category_ID = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
     owner_ID = models.ForeignKey(User_Profile, null=True, on_delete=models.CASCADE)
@@ -44,7 +47,8 @@ class Item(models.Model):
     rating = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     rental_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     date_listed = models.DateTimeField(auto_now=True)
-    picture = models.FileField(null=True)
+    picture = models.ImageField(null=True, upload_to='itemImages')
+    
     def __str__(self):
         return self.name
 
--- anyWares/urls.py
+++ anyWares/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search$', views.search, name='search'),
-    url(r'^itemview$', views.itemView, name='itemView'),
+    url(r'^itemview$', views.itemView, name='itemView')
 ]
