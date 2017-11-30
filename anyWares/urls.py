a823288381623bdd08923fb067330cfe7f5d02db
Ryan Bass <stripedbass511@gmail.com>
2017-11-29 19:46:28 -0500

    Added user login page

--- anyWares/urls.py
+++ anyWares/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -9,4 +10,6 @@ urlpatterns = [
     url(r'^search$', views.search, name='search'),
     url(r'^itemView/(?P<item_id>[0-9]+)/$', views.itemView, name='itemView'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
