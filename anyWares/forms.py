59ee8c5fe5ffc2eaea6215f3f9bb7c73a6b72acc
Ryan Bass <stripedbass511@gmail.com>
2017-12-03 15:35:49 -0500

    Added login function, made create item page functional

--- anyWares/forms.py
+++ anyWares/forms.py
@@ -1,11 +1,6 @@
 from django import forms
-from anyWares.models import Category
+from anyWares.models import Item
+from django.forms import ModelForm
 
-class NewItemForm(forms.Form):
-    item_name = forms.CharField(label='Item name', max_length=100)
-    item_category = forms.ModelChoiceField(queryset=Category.objects.all())
-    item_price = forms.IntegerField(max_value=None, min_value=0)
-    item_availability_start = forms.DateField()
-    item_availability_end = forms.DateField()
-    item_image = forms.ImageField()
-    item_pickup_location = forms.CharField(max_length=100)
+class ImageUploadForm(forms.Form):
+    image = forms.ImageField()
