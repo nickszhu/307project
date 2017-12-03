c1d9853e28f55c93b8de2a7db25706a43834a7f4
Ryan Bass <stripedbass511@gmail.com>
2017-12-03 12:55:22 -0500

    4

--- anyWares/forms.py
+++ anyWares/forms.py
@@ -3,3 +3,9 @@ from anyWares.models import Category
 
 class NewItemForm(forms.Form):
     item_name = forms.CharField(label='Item name', max_length=100)
+    item_category = forms.ModelChoiceField(queryset=Category.objects.all())
+    item_price = forms.IntegerField(max_value=None, min_value=0)
+    item_availability_start = forms.DateField()
+    item_availability_end = forms.DateField()
+    item_image = forms.ImageField()
+    item_pickup_location = forms.CharField(max_length=100)
