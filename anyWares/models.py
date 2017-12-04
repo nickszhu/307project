4699b9058f43cee060f3c338ec06debdd95941d0
Ryan Bass <stripedbass511@gmail.com>
2017-12-04 16:31:15 -0500

    6

--- anyWares/models.py
+++ anyWares/models.py
@@ -33,13 +33,10 @@ class Profile(models.Model):
         return str(self.user)
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.profile.save()  
 
 class Category(models.Model):
     category_name = models.CharField(max_length=20)
